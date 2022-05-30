447e7a549da8b533c4d2bb43fbd405f37da03733
netra <netra@netras-mbp.localdomain>
2022-05-29 22:32:42 -0700

    Comment in AmazonLogin class

--- google-test/src/main/java/sample/AmazonLogin.java
+++ google-test/src/main/java/sample/AmazonLogin.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
-
+// this is new comment
 public class AmazonLogin {
 
 	public static void main(String[] args) throws InterruptedException {
